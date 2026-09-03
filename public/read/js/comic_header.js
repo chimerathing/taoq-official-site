751ddcf028e244ec9fdc227469d727a078e4b981
chimerathing <natarey0922@gmail.com>
2026-09-03 19:04:26 -0400

    js

--- public/read/js/comic_header.js
+++ public/read/js/comic_header.js
@@ -6,12 +6,12 @@ document.querySelector(".writeHeader").innerHTML = `
         <a href="home.html"><img src="https://amazingobjectquiz.neocities.org/assets/banner%20logo.png" alt="" id="banner"/></a> 
 
         <div id="nav">
-            <a href="home.html">HOME</a> |
-            <a href="read.html">READ</a> |
-            <a href="archive.html">ARCHIVE</a> |
-             <a href="cast.html">CAST</a> |
-            <a href="about.html">ABOUT</a> |
-            <a href="extras.html">EXTRAS</a> |
+            <a href="home.html">HOME</a> ★
+            <a href="read.html">READ</a> ★
+            <a href="archive.html">ARCHIVE</a> ★
+             <a href="cast.html">CAST</a> ★
+            <a href="about.html">ABOUT</a> ★
+            <a href="extras.html">EXTRAS</a> ★
              <a href="news.html">NEWS</a>
         </div>
     </header>
