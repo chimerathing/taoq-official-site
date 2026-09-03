3c8ccfe72979ead5340dabf8502ac6a738330cc1
chimerathing <natarey0922@gmail.com>
2026-09-02 21:07:36 -0400

    hi

--- public/read/js/comic_header.js
+++ public/read/js/comic_header.js
@@ -1,8 +1,9 @@
 //the header of the site would be handled in this javascript file, so you don't have to copypaste the whole thing onto every page.
 //at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeHeader"
 document.querySelector(".writeHeader").innerHTML = `
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all">
     <header align="center">
-        <a href="home.html"><img src="https://amazingobjectquiz.neocities.org/assets/banner%20logo.png" alt="" /></a> 
+        <a href="home.html"><img src="https://amazingobjectquiz.neocities.org/assets/banner%20logo.png" alt="" id="banner"/></a> 
 
         <div id="nav">
             <a href="home.html">HOME</a> |
